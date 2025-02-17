67ce379ad2e0e419dc50cf6798a959adf0ff4676
WizenBike <roman.studenc@gmail.com>
2025-02-17 14:47:01 +0100

    Update

---
+++ Assets/Scripts/FreezeRotation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeRotation : MonoBehaviour
+{
+    public Transform child;
+    void Update()
+    {
+        child.transform.rotation = Quaternion.Euler(0.0f, 0.0f, gameObject.transform.rotation.z * -1.0f);
+    }
+}
